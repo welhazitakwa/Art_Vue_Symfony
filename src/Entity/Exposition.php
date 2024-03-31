3b3cefbe66957998be27dceca7e28575858fa35e
khalil27 <khalil.taallah@esprit.tn>
2024-03-31 16:25:26 +0200

    test

--- src/Entity/Exposition.php
+++ src/Entity/Exposition.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Categorie;
 
 /**
  * Exposition
@@ -12,6 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Exposition
 {
+
     /**
      * @var int
      *
