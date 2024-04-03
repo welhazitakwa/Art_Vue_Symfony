403b62c870b369fb2c23eaf3391a258b7c73b76c
arijabidi <arijabidi577@gmail.com>
2024-04-03 17:06:38 +0100

    admincrud

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -2,124 +2,97 @@
 
 namespace App\Entity;
 
+use App\Repository\UtilisateurRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * Utilisateur
- *
- * @ORM\Table(name="utilisateur", uniqueConstraints={@ORM\UniqueConstraint(name="login", columns={"login"}), @ORM\UniqueConstraint(name="email", columns={"email"})})
- * @ORM\Entity
- */
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+
 class Utilisateur
 {
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: "IDENTITY")]
+    #[ORM\Column(type: "integer", nullable: false)]
     private $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nom", type="text", length=65535, nullable=false)
-     */
+    #[ORM\Column(type: "text", length: 65535, nullable: false)]
     private $nom;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="prenom", type="text", length=65535, nullable=false)
-     */
+    #[ORM\Column(type: "text", length: 65535, nullable: false)]
     private $prenom;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=100, nullable=false)
-     */
+    #[ORM\Column(type: "string", length: 100, nullable: false)]
     private $email;
 
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="numTel", type="integer", nullable=true)
-     */
+    #[ORM\Column(type: "integer", nullable: true)]
     private $numtel;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="login", type="string", length=100, nullable=false)
-     */
+    #[ORM\Column(type: "string", length: 100, nullable: false)]
     private $login;
 
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="cin", type="integer", nullable=true)
-     */
+    #[ORM\Column(type: "integer", nullable: true)]
     private $cin;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="mdp", type="text", length=65535, nullable=false)
-     */
+    #[ORM\Column(type: "text", length: 65535, nullable: false)]
     private $mdp;
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="profil", type="integer", nullable=false)
-     */
+    #[ORM\Column(type: "integer", nullable: false)]
     private $profil;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="image", type="text", length=65535, nullable=true)
-     */
+    #[ORM\Column(type: "text", length: 65535, nullable: true)]
     private $image;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="genre", type="text", length=65535, nullable=true)
-     */
+    #[ORM\Column(type: "text", length: 65535, nullable: true)]
     private $genre;
 
-    /**
-     * @var \DateTime|null
-     *
-     * @ORM\Column(name="dateNaissance", type="date", nullable=true)
-     */
+    #[ORM\Column(type: "date", nullable: true)]
     private $datenaissance;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="adresse", type="text", length=65535, nullable=true)
-     */
+    #[ORM\Column(type: "text", length: 65535, nullable: true)]
     private $adresse;
 
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="date_inscription", type="date", nullable=false)
-     */
+    #[ORM\Column(type: "date", nullable: false)]
     private $dateInscription;
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="etat_compte", type="integer", nullable=false)
-     */
+    #[ORM\Column(type: "integer", nullable: false)]
     private $etatCompte;
 
-    
+    #[ORM\ManyToMany(targetEntity: Evenement::class, mappedBy: 'utilisateurs')]
+    private Collection $Evenement;
 
+
+    public function __construct()
+    {
+        $this->Evenement = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection<int, Evenement>
+     */
+    public function getEvenement(): Collection
+    {
+        return $this->Evenement;
+    }
+
+    public function addEvenement(Evenement $evenement): static
+    {
+        if (!$this->Evenement->contains($evenement)) {
+            $this->Evenement->add($evenement);
+        }
+
+        return $this;
+    }
+
+    public function removeEvenement(Evenement $evenement): static
+    {
+        $this->Evenement->removeElement($evenement);
+
+        return $this;
+    }
+    public function __toString(): string
+    {
+        return $this->nom . ' (' . $this->email . ')';
+    }       
 
 }
---
+++ src/Form/EvenementType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evenement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvenementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('lieu')
+            ->add('date')
+            ->add('price')
+            ->add('capacite')
+            ->add('calender')
+            ->add('utilisateurs')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Evenement::class,
+        ]);
+    }
+}
