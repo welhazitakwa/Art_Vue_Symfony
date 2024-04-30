1223b722c9e24c7c15be10f27523e9a773a8b6b8
khalil27 <khalil.taallah@esprit.tn>
2024-04-30 23:17:04 +0200

    r

--- src/Entity/Exposition.php
+++ src/Entity/Exposition.php
@@ -2,62 +2,96 @@
 
 namespace App\Entity;
 
+use App\Repository\ExpositionRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * Exposition
- *
- * @ORM\Table(name="exposition", indexes={@ORM\Index(name="id_utilisateur", columns={"id_utilisateur"})})
- * @ORM\Entity
- */
+#[ORM\Entity(repositoryClass: ExpositionRepository::class)]
 class Exposition
 {
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    private $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nom", type="string", length=55, nullable=false)
-     */
-    private $nom;
+    #[ORM\Column(type: 'string', length: 55)]
+    private string $nom;
 
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="dateDebut", type="date", nullable=false)
-     */
-    private $datedebut;
+    #[ORM\Column(type: 'date')]
+    private \DateTimeInterface $datedebut;
 
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="dateFin", type="date", nullable=false)
-     */
-    private $datefin;
+    #[ORM\Column(type: 'date')]
+    private \DateTimeInterface $datefin;
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="nbrOeuvre", type="integer", nullable=false)
-     */
-    private $nbroeuvre;
+    #[ORM\Column(type: 'integer')]
+    private int $nbroeuvre;
 
-    /**
-     * @var \Utilisateur
-     *
-     * @ORM\ManyToOne(targetEntity="Utilisateur")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="id_utilisateur", referencedColumnName="id")
-     * })
-     */
-    private $idUtilisateur;
+    #[ORM\ManyToOne(targetEntity: Utilisateur::class)]
+    #[ORM\JoinColumn(name: 'id_utilisateur', referencedColumnName: 'id')]
+    private ?Utilisateur $idUtilisateur;
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDatedebut(): \DateTimeInterface
+    {
+        return $this->datedebut;
+    }
+
+    public function setDatedebut(\DateTimeInterface $datedebut): self
+    {
+        $this->datedebut = $datedebut;
+
+        return $this;
+    }
+
+    public function getDatefin(): \DateTimeInterface
+    {
+        return $this->datefin;
+    }
+
+    public function setDatefin(\DateTimeInterface $datefin): self
+    {
+        $this->datefin = $datefin;
+
+        return $this;
+    }
+
+    public function getNbroeuvre(): int
+    {
+        return $this->nbroeuvre;
+    }
+
+    public function setNbroeuvre(int $nbroeuvre): self
+    {
+        $this->nbroeuvre = $nbroeuvre;
+
+        return $this;
+    }
+
+    public function getIdUtilisateur(): ?Utilisateur
+    {
+        return $this->idUtilisateur;
+    }
+
+    public function setIdUtilisateur(?Utilisateur $idUtilisateur): self
+    {
+        $this->idUtilisateur = $idUtilisateur;
+
+        return $this;
+    }
 }
